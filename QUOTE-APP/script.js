2834cad9e438cea34ddf75f63db98d295c3126cd
Christine Maria Metz <Christine.maria.metz@gmail.com>
2023-06-13 15:49:18 +0200

    Ordner Struktur angepasst & Quote-App angefangen

---
+++ QUOTE-APP/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/* QUOTE APP
+*/
+
+// Welches Element muss ich bedienen können? (Button)
+// ---> Variablen bauen
+const getQuote = document.querySelector("#get-quote");
+
+//fetch("https://dummy-apis.netlify.app/api/quote");
